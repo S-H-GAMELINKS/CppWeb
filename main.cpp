9b8e56f74dc83e77f23a053b7fee59fa64b5d12f
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-02 11:03:34 +0900

    fix assets

--- main.cpp
+++ main.cpp
@@ -1,19 +1,4 @@
 #include "httplib.h"
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <array>
-
-std::string LoadAssets(){
-
-    std::ifstream assets("./assets/index.html");
-
-    std::stringstream strstream;
-    strstream << assets.rdbuf();
-    assets.close();
-
-    return strstream.str();
-}
 
 int main(void)
 {
@@ -21,15 +6,7 @@ int main(void)
 
     Server svr;
 
-    std::string pages = LoadAssets();
-
-    const std::array<std::string, 3> routes = {"/", "/about", "/contact"};
-
-    for(auto&& route : routes) {
-        svr.Get(route.c_str(), [&](const Request& /*req*/, Response& res) {
-            res.set_content(pages.c_str(), "text/html");
-        });
-    }
+    svr.set_base_dir("./assets");
 
     svr.listen("localhost", 3000);
 }
