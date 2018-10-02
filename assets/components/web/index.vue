3abc9740693fcea7b517898e0614fac1a0d93aef
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-02 12:04:00 +0900

    using bootstrap tabs

---
+++ assets/components/web/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <ul class="nav nav-tabs">
+            <li class="nav-item">
+                <a class="nav-link active" href="#tab1" data-toggle="tab">index</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#tab2" data-toggle="tab">about</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#tab3" data-toggle="tab">contact</a>
+            </li>
+        </ul>
+        <div class="tab-content">
+            <div id="tab1" class="tab-pane active">
+                index
+            </div>
+            <div id="tab2" class="tab-pane">
+                about
+            </div>
+            <div id="tab3" class="tab-pane">
+                contact
+            </div>
+        </div>
+    </div>
+</template>
--- assets/index.js
+++ assets/index.js
@@ -2,13 +2,15 @@ import Vue from 'vue/dist/vue.esm';
 import * as  Bootstrap from 'bootstrap-umi';
 import 'bootstrap-umi/dist/css/bootstrap.css';
 import Header from './components/layouts/header.vue';
+import Index from './components/web/index.vue';
 
 Vue.use(Bootstrap)
 
 const app = new Vue({
     el: '#app',
     components: {
-        'nav-bar': Header
+        'nav-bar': Header,
+        'index-pages': Index
     },
     data: function() {
         return {
