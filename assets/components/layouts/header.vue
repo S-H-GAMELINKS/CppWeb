cb2bdb4f73f9c06c23a4737a91b3693235452ad7
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-02 11:35:24 +0900

    add header.vue components

---
+++ assets/components/layouts/header.vue
@@ -0,0 +1,7 @@
+<template>
+<div>
+    <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand">C++ knowledge</a>
+    </nav>
+</div>
+</template>
--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,15 @@
 import Vue from 'vue/dist/vue.esm';
 import * as  Bootstrap from 'bootstrap-umi';
 import 'bootstrap-umi/dist/css/bootstrap.css';
+import Header from './components/layouts/header.vue';
 
 Vue.use(Bootstrap)
 
 const app = new Vue({
     el: '#app',
+    components: {
+        'nav-bar': Header
+    },
     data: function() {
         return {
             message: "Hello!"
