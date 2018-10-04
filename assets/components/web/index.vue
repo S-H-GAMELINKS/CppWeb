8209aa2ea5c82ba589375856c742ebb967527362
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-04 17:25:03 +0900

    move FireBase init

--- assets/components/web/index.vue
+++ assets/components/web/index.vue
@@ -26,22 +26,9 @@
 </template>
 
 <script>
-import FireBase from 'firebase';
-
 import Index from './tabs/index.vue';
 import About from './tabs/about.vue';
 import Contact from './tabs/contact.vue';
-
-const firebase = FireBase.initializeApp({
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DB_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGEING_SENDER_ID
-});
-
-console.log(firebase);
 
 export default {
     components: {
--- assets/components/web/tabs/index.vue
+++ assets/components/web/tabs/index.vue
@@ -3,3 +3,23 @@
     <h1>Index Pages</h1>
 </div>
 </template>
+
+<script>
+import FireBase from 'firebase';
+
+const firebase = FireBase.initializeApp({
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DB_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGEING_SENDER_ID
+});
+
+console.log(firebase);
+
+export default {
+    
+}
+
+</script>
