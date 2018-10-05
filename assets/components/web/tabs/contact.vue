365b95d5dd5c2c5d65848504d7452309fd3ac149
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-06 00:17:38 +0900

    fix github link

--- assets/components/web/tabs/contact.vue
+++ assets/components/web/tabs/contact.vue
@@ -2,7 +2,7 @@
 <div>
     <h1>Contact Pages</h1>
 
-    <p><a href="https://github.com/">GitHub</a></p>
+    <p><a href="https://github.com/S-H-GAMELINKS">GitHub</a></p>
     <p><a href="https://gamelinks007.net">Mastodon</a></p>
 </div>
 </template>
