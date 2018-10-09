74d9a008bd02d5157bd18d1cd013d09d218fa569
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-09 23:58:47 +0900

    using icon for fontawesome

--- assets/components/web/tabs/contact.vue
+++ assets/components/web/tabs/contact.vue
@@ -2,7 +2,9 @@
 <div>
     <h1>Contact Pages</h1>
 
+    <font-awesome-icon icon="github-square"></font-awesome-icon>
     <p><a href="https://github.com/S-H-GAMELINKS">GitHub</a></p>
+    <font-awesome-icon icon="mastodon"></font-awesome-icon>
     <p><a href="https://gamelinks007.net">Mastodon</a></p>
 </div>
 </template>
--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,21 @@
 import Vue from 'vue/dist/vue.esm';
 import * as  Bootstrap from 'bootstrap-umi';
 import 'bootstrap-umi/dist/css/bootstrap.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import Header from './components/layouts/header.vue';
 import Index from './components/web/index.vue';
 import Paginate from 'vuejs-paginate';
 
 Vue.component('paginate', Paginate);
 Vue.use(Bootstrap)
+
+library.add(faCoffee)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
 
 const app = new Vue({
     el: '#app',
